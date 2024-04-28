ad5d3d336793209db69c2e52d7a7e90182ecce10
mercuricchloride <mercuryprotocol@protonmail.com>
2024-04-27 20:21:37 -0700

    Install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# set the latest release
+LATEST_RELEASE="https://github.com/MercuricChloride/streamline-cli/releases/latest"
+
+# Check for rustup
+if ! command -v rustup > /dev/null 2>&1; then
+    echo "Rust isn't installed on your system. Please go to: https://rustup.rs/ to install rust!"
+    exit 1
+fi
+
+# Check for homebrew
+if ! command -v brew > /dev/null 2>&1; then
+    echo "Homebrew isn't installed on your system. Please go to: https://brew.sh/ to install the homebrew package manager!"
+    exit 1
+fi
+
+# Install the dependencies
+brew install bufbuild/buf/buf protobuf gawk streamingfast/tap/substreams
+
+# Install the wasm target for rust
+rustup target add wasm32-unknown-unknown
+
+# Make the abis directory
+mkdir -p "$HOME/streamline-cli/abis"
+
+# Clone the template repo down into the template-repo dir
+git clone "https://github.com/MercuricChloride/streamline-template-repository.git" "$HOME/streamline-cli/template-repo"
+
+# Pull the tarball from the latest github release
+curl -L -o "/tmp/scripts.tar.gz" "$LATEST_RELEASE/download/scripts.tar.gz"
+
+# Unpack the tarball into the streamline-cli dir
+tar -xzf "/tmp/scripts.tar.gz" "$HOME/streamline-cli"
